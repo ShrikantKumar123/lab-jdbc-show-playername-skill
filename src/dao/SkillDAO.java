1bfb73c6c157c828a1b41fd0d8ced62f549f7997
unknown <shrikant2994anil@gmail.com>
2020-04-09 22:34:44 +0530

    FSJ0320A005

--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,44 @@
 
+package dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	 public Skill getSkillByID(Long id) throws ClassNotFoundException, SQLException, IOException 
+	{
+		 Connection con=ConnectionManager.getConnection();
+		 
+		 Statement statement=  con.createStatement();
+			
+			ResultSet rs =statement.executeQuery("select * from skills ");
+			
+			Skill skill=new Skill();
+			while(rs.next())
+			{
+				long checkid=rs.getLong(1);
+				if(checkid==id)
+				{
+				
+				String name=rs.getString(2);
+				skill.setSkilld(checkid);
+				skill.setSkillName(name);
+				
+				return skill;
+				}
+			
+			}
+		
+		return null;
+	 
+	}
+	 
+}
+
